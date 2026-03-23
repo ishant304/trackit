ac4c53fa597620f934d42e53d1088fa842eba219
Ishant Srivastava <ishantishu125@gmail.com>
2026-03-24 01:45:11 +0530

    24-4

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,10 @@ connectDB();
 const app = express()
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://trackit-wine.vercel.app"
+  ],
   credentials: true
 }));
 
